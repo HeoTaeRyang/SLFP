72370e8195c66e5f8c1f40fb33d35bf0dcdea6ea
HeoTeaRyang <cake0702@naver.com>
2024-12-04 23:37:53 +0900

    경기 테이블 컨트롤 기능

--- backend/db/game.py
+++ backend/db/game.py
@@ -1,1 +1,26 @@
 from db.connect import con
+
+def get_game_team_win_num(year,team):
+    cursor = con.cursor()
+    cursor.execute(f"SELECT count(*) from Game where year = '{year}' and ((home_team = '{team}' and home_result = '승') or (away_team = '{team}' and home_result = '패'));")
+    return cursor.fetchone()[0]
+
+def get_game_team_lose_num(year,team):
+    cursor = con.cursor()
+    cursor.execute(f"SELECT count(*) from Game where year = '{year}' and ((home_team = '{team}' and home_result = '패') or (away_team = '{team}' and home_result = '승'));")
+    return cursor.fetchone()[0]
+
+def get_game_team_draw_num(year,team):
+    cursor = con.cursor()
+    cursor.execute(f"SELECT count(*) from Game where year = '{year}' and (home_team = '{team}'or away_team = '{team}') and home_result = '무';")
+    return cursor.fetchone()[0]
+
+def get_game_month(year,month):
+    cursor = con.cursor()
+    cursor.execute(f"SELECT * from Game WHERE year = '{year}' and date like '{month}월%';")
+    return cursor.fetchall()
+
+def get_game_team_result(year,team):
+    cursor = con.cursor()
+    cursor.execute(f"SELECT * from Game where year = '{year}' and (home_team = '{team}' or away_team = '{team}');")
+    return cursor.fetchall()
