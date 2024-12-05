32c92ad0017f3b2b228cac3a62a071cbe4ff08fa
HeoTeaRyang <cake0702@naver.com>
2024-12-05 20:12:41 +0900

    오류 수정

--- backend/db/record.py
+++ backend/db/record.py
@@ -7,5 +7,5 @@ def get_record_batter(date,team,dh):
 
 def get_record_pitcher(date,team,dh):
     cursor = con.cursor()
-    cursor.execute(f"SELECT * FROM Record_pitcher WHERE date  = '{date}' and team='{team}' and (dh = '{dh}' or dh isnone);")
+    cursor.execute(f"SELECT * FROM Record_pitcher WHERE date  = '{date}' and team='{team}' and dh = '{dh}';")
     return cursor.fetchall()
